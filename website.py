cbfbfa603990b48e47c949533df33edcd93da64b
Jeao13 <136776317+Jeao13@users.noreply.github.com>
2023-11-08 19:12:42 +0800

    Update website.py

--- website.py
+++ website.py
@@ -32,11 +32,11 @@ import threading
 start_time = time.time()
 
 db_config = {
-    'host': os.environ.get('MYSQL_ADDON_DIRECT_HOST', 'hv-sgp1-002.clvrcld.net'),
+    'host': os.environ.get('MYSQL_ADDON_DIRECT_HOST', 'hv-sgp1-001.clvrcld.net'),
     'user': os.environ.get('MYSQL_ADDON_USER', 'uhmp5ztvzx8cl2iq'),
     'password': os.environ.get('MYSQL_ADDON_PASSWORD', 'FUdxr6xyK2ZGGjrmZjS'),
     'database': os.environ.get('MYSQL_ADDON_DB', 'b28tqgc0yz0kvrdrsv6r'),
-    'port': os.environ.get('MYSQL_ADDON_DIRECT_PORT', '10108'),
+    'port': os.environ.get('MYSQL_ADDON_DIRECT_PORT', '10269'),
 }
 
 try:
